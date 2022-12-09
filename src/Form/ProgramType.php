eee426716ea24c813af36d0f354f29a866ef2790
Nicolas Montard <montardnico@gmail.com>
2022-12-09 14:41:40 +0100

    quete 19

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -18,6 +20,13 @@ class ProgramType extends AbstractType
             ->add('synopsis', TextareaType::class)
             ->add('poster', FileType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
         ;
     }
 
