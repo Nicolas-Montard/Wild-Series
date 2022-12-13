a8abd9d4d708ecdb657aa49ff8975edd5fd4721d
Nicolas Montard <montardnico@gmail.com>
2022-12-13 12:50:27 +0100

    quete 20

--- src/Service/ProgramDuration.php
+++ src/Service/ProgramDuration.php
@@ -8,12 +8,24 @@ class ProgramDuration
 {
     public function calculate(Program $program): string
     {
-        $length = 0;
+        $minutes = 0;
         foreach ($program->getSeasons() as $season) {
             foreach ($season->getEpisodes() as $episode) {
-                $length += $episode->getDuration();
+                $minutes += $episode->getDuration();
             }
         }
-        return strval($length);
+        $days=0;
+        if ($minutes > 1440) {
+        $days = floor($minutes / 1440);
+        }
+        $hours = floor(($minutes - ($days * 1440)) / 60);
+        $minutes -= (($days * 1440) + ($hours * 60));
+
+        $times = "";
+        if($days !== 0) {
+            $times = $days . " jours ";
+        }
+        $times .= $hours . ' heures et ' . $minutes . ' minutes';
+        return $times;
     }
 }
