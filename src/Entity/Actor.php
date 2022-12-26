df13df5a2a102332245e3702946c5725e5c3c60e
Nicolas_Montard <montardnico@gmail.com>
2022-12-26 13:01:18 +0100

    quete-22

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -3,11 +3,18 @@
 namespace App\Entity;
 
 use App\Repository\ActorRepository;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[Vich\Uploadable]
 class Actor
 {
     #[ORM\Id]
@@ -20,6 +27,21 @@ class Actor
 
     #[ORM\ManyToMany(targetEntity: Program::class, inversedBy: 'actors')]
     private Collection $programs;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $poster = null;
+
+    #[Vich\UploadableField(mapping: 'poster_file', fileNameProperty: 'poster')]
+    #[Assert\File(
+        maxSize: '1M',
+        mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],
+        )]
+    private ?File $posterFile = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?DateTimeInterface $updatedAt = null;
+
+
 
     public function __construct()
     {
@@ -63,6 +85,69 @@ class Actor
     public function removeProgram(Program $program): self
     {
         $this->programs->removeElement($program);
+
+        return $this;
+    }
+
+    /**
+     * Get the value of posterFile
+     */ 
+    public function getPosterFile()
+    {
+        return $this->posterFile;
+    }
+
+    /**
+     * Set the value of posterFile
+     *
+     * @return  self
+     */ 
+    public function setPosterFile($posterFile)
+    {
+        $this->posterFile = $posterFile;
+        if($posterFile){
+            $this->updatedAt = new DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updatedAt
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of poster
+     */ 
+    public function getPoster()
+    {
+        return $this->poster;
+    }
+
+    /**
+     * Set the value of poster
+     *
+     * @return  self
+     */ 
+    public function setPoster($poster)
+    {
+        $this->poster = $poster;
 
         return $this;
     }
