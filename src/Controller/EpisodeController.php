2776a90d51cd1d70005978c19ba5328d78dbd4ca
Nicolas_Montard <montardnico@gmail.com>
2022-12-28 11:50:54 +0100

    quete-23

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Episode;
+use App\Form\CommentType;
 use App\Form\EpisodeType;
+use App\Repository\CommentRepository;
 use App\Repository\EpisodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -55,11 +58,28 @@ class EpisodeController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'app_episode_show', methods: ['GET'])]
-    public function show(Episode $episode): Response
+    #[Route('/{slug}', name: 'app_episode_show', methods: ['GET', 'POST'])]
+    public function show(Episode $episode, Request $request, CommentRepository $commentRepository): Response
     {
-        return $this->render('episode/show.html.twig', [
+        $comment = new Comment;
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setAuthor($this->getUser());
+            $comment->setEpisode($episode);
+            $commentRepository->save($comment, true);
+
+            $this->addFlash('success', 'Your comment has been created');
+
+            return $this->redirectToRoute('app_episode_show', ['slug' => $episode->getSlug()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('episode/show.html.twig', [
             'episode' => $episode,
+            'form' => $form,
+            'comments' => $commentRepository->findBy(['episode' => $episode->getId() ]),
         ]);
     }
 
